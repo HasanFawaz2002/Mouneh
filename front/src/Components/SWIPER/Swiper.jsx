3bc92fa138228225d8d77fa26f26aa5486c27afb
unknown <hasan.amaz2002@gmail.com>
2023-09-24 10:11:50 +0300

    Front

--- front/src/Components/SWIPER/Swiper.jsx
+++ front/src/Components/SWIPER/Swiper.jsx
@@ -21,7 +21,7 @@ const Swiper = () => {
     }
   },[swiperIsInView])
   useEffect(() => {
-    axios.get('http://localhost:3001/newProduct')
+    axios.get('https://mouneh-deploy.onrender.com/newProduct')
       .then(result => setNewProduct(result.data))
       .catch(error => console.error(error));
   }, []);
